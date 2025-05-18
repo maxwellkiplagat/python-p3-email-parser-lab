897b97763cd2429ea3142ec3ba2f8bac58bfc83c
maxwellkiplagat <maxwellkiplagat0@gmail.com>
2025-05-18 20:56:12 +0300

    completed email-parser

--- lib/email_address_parser.py
+++ lib/email_address_parser.py
@@ -1,1 +1,13 @@
-# your code goes here!
+import re
+
+class EmailAddressParser:
+    def __init__(self, email_addresses):
+        self.email_addresses = email_addresses
+
+    def parse(self):
+        # Match only valid email addresses using regex
+        email_pattern = r'[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}'
+        matches = re.findall(email_pattern, self.email_addresses)
+
+        # Remove duplicates and sort alphabetically
+        return sorted(set(matches))
